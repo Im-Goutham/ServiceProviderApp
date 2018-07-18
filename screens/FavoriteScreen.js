6300660221b58c6dec54aadd28270f1ba8e47446
Goutham <gouthamrs4u@gmail.com>
2018-07-18 17:02:29 +0530

    React i18n setup done for android and ios

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'native-base';
 import RNCalendarEvents from 'react-native-calendar-events';
+import  I18n, { getLanguages } from 'react-native-i18n';
 
 import Header from '../components/Header';
 
 
 class FavoriteScreen extends Component {
     
+    componentDidMount(){
+        getLanguages().then(languages => {
+            console.log(languages); // ['en-US', 'en']
+          });
+    }
+
     getEvents = () => {
         console.log(RNCalendarEvents.saveEvent('Calender Test', {id: 'FE6B128F-C0D8-4FB8-8FC6-D1D6BA015CDE'}));
     }
@@ -17,9 +24,9 @@ class FavoriteScreen extends Component {
            <View style={{flex:1}}>
            <Header navigation={this.props.navigation}  title={'Favorite'}/>
              <View style={styles.container}>
+                 <Text>{I18n.t('greeting')}</Text>
                  <Text>FavoriteScreen</Text>
                  <Text>FavoriteScreen</Text>
-                 <Text>FavoriteScreen1111</Text>
                  <Button onPress={this.getEvents}>
                         <Text>Click Me!</Text>
                 </Button>
@@ -30,6 +37,17 @@ class FavoriteScreen extends Component {
 }
 
 
+I18n.fallbacks = true;
+
+I18n.translations = {
+  en: {
+    greeting: 'Hi!',
+  },
+  'hi-IN': {
+    greeting: 'Bonjour!',
+  },
+};
+
 const styles = StyleSheet.create({
       container: {
          flex: 1,
@@ -37,5 +55,7 @@ const styles = StyleSheet.create({
          alignItems: 'center'
     }
 })
+
+
 
 export default FavoriteScreen;
